4aaf56f14f6067c01a4a942b487717e563e3c393
Roderick Monje <rod@foveacentral.com>
2021-03-04 13:50:32 -0500

    fix: remove location#id, run migration

--- db/migrate/20210304183621_create_locations.rb
+++ db/migrate/20210304183621_create_locations.rb
@@ -1,7 +1,6 @@
 class CreateLocations < ActiveRecord::Migration[6.1]
   def change
     create_table :locations do |t|
-      t.string :id
       t.string :x_parent
       t.integer :num_children
       t.boolean :inactive
---
+++ db/schema.rb
@@ -0,0 +1,44 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_03_04_183621) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "x_parent"
+    t.integer "num_children"
+    t.boolean "inactive"
+    t.string "organization"
+    t.string "name"
+    t.string "addr1"
+    t.string "addr2"
+    t.string "vaccines"
+    t.string "logo"
+    t.string "map_zoom"
+    t.string "notes"
+    t.string "notes_spn"
+    t.string "alt"
+    t.string "alt_spn"
+    t.string "date"
+    t.string "time"
+    t.string "link"
+    t.string "second_dose"
+    t.string "full"
+    t.string "comments"
+    t.string "comments_spn"
+    t.string "times"
+    t.string "clinic_format"
+    t.string "system"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end

4348815e6cce0140669a2da3d635e0d0c9e08dc9
Roderick Monje <rod@foveacentral.com>
2021-03-04 13:36:45 -0500

    sys: added location
    rails g model Location id:string x_parent:string num_children:integer inactive:boolean organization:string name:string addr1:string addr2:string vaccines:string logo:string map_zoom:string notes:string notes_spn:string alt:string alt_spn:string date:string time:string link:string second_dose:string full:string comments:string comments_spn:string times:string clinic_format:string system:string

---
+++ db/migrate/20210304183621_create_locations.rb
@@ -0,0 +1,33 @@
+class CreateLocations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :locations do |t|
+      t.string :id
+      t.string :x_parent
+      t.integer :num_children
+      t.boolean :inactive
+      t.string :organization
+      t.string :name
+      t.string :addr1
+      t.string :addr2
+      t.string :vaccines
+      t.string :logo
+      t.string :map_zoom
+      t.string :notes
+      t.string :notes_spn
+      t.string :alt
+      t.string :alt_spn
+      t.string :date
+      t.string :time
+      t.string :link
+      t.string :second_dose
+      t.string :full
+      t.string :comments
+      t.string :comments_spn
+      t.string :times
+      t.string :clinic_format
+      t.string :system
+
+      t.timestamps
+    end
+  end
+end

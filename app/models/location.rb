cea77af01eae9f46db0fee4a87793fe1591872e9
Roderick Monje <rod@foveacentral.com>
2021-03-04 16:46:32 -0500

    f(x): Location accessors

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,10 @@
 class Location < ApplicationRecord
+  alias_attribute :xParent, :x_parent
+  alias_attribute :NumChildren, :num_children
+  alias_attribute :mapZoom, :map_zoom
+  alias_attribute :notesSpn, :notes_spn
+  alias_attribute :altSpn, :alt_spn
+  alias_attribute :secondDose, :second_dose
+  alias_attribute :commentsSpn, :comments_spn
+  alias_attribute :clinicFormat, :clinic_format
 end

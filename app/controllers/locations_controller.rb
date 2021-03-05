4c5b9bbf1a5c7ecfcec8ef9b993b9f99de376907
Roderick Monje <rod@foveacentral.com>
2021-03-05 16:42:26 -0500

    fix: rubocop -A

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class LocationsController < ApplicationController
   require 'net/http'
 
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Location < ApplicationRecord
   alias_attribute :xParent, :x_parent
   alias_attribute :NumChildren, :num_children

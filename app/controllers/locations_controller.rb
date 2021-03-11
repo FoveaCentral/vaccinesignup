57e31e7efd060eb6f6caf3cc2e543ba41a34b3a0
Roderick Monje <rod@foveacentral.com>
2021-03-11 18:05:50 -0500

    refactor: extracted LocationSyncer service

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,27 +2,9 @@
 
 # Controller for managing Covid-19 vaccine Locations.
 class LocationsController < ApplicationController
-  require 'net/http'
-
-  LA_URL = 'http://publichealth.lacounty.gov/acd/ncorona2019/js/pod-data.js'
-
   # Syncs vaccine Locations with LA County's data set.
   def sync
-    new = 0
-    location_a = la_locations
-    location_a.each do |location_h|
-      id = location_h.delete('id')
-      location = Location.find_by_best_key(id, location_h['addr1']) || Location.new
-      new += 1 if location.new_record?
-      location.update(location_h)
-    end
-    render plain: ["Parsed #{location_a.size} locations.", "Created #{new} locations."].join("\n")
-  end
-
-  private
-
-  def la_locations
-    response = Net::HTTP.get(URI(LA_URL)).gsub('var unfiltered = ', '')
-    JSON.parse(response)
+    results = LocationSyncer.call
+    render plain: ["Parsed #{results[:locations]} locations.", "Created #{results[:new_locations]} locations."].join("\n")
   end
 end
---
+++ app/services/location_syncer.rb
@@ -0,0 +1,24 @@
+require 'net/http'
+
+# Syncs vaccine Locations with LA County's data set.
+class LocationSyncer < ApplicationService
+  LA_URL = 'http://publichealth.lacounty.gov/acd/ncorona2019/js/pod-data.js'
+
+  def call
+    new = 0
+    locations = la_locations.each do |location_h|
+      id = location_h.delete('id')
+      location = Location.find_by_best_key(id, location_h['addr1']) || Location.new
+      new += 1 if location.new_record?
+      location.update(location_h)
+    end.size
+    { locations: locations, new_locations: new }
+  end
+
+  private
+
+  def la_locations
+    response = Net::HTTP.get(URI(LA_URL)).gsub('var unfiltered = ', '')
+    JSON.parse(response)
+  end
+end

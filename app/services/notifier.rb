4bb3363a9078ed17f4c95ae3115c1b9068dbff2e
Roderick Monje <rod@foveacentral.com>
2021-03-12 01:18:26 -0500

    fix: rubocop

--- app/services/notifier.rb
+++ app/services/notifier.rb
@@ -3,18 +3,24 @@
 # Notifies users about available appointments in the zips they follow.
 class Notifier < ApplicationService
   def call
-    clinics = 0
-    users = 0
-    UserZip.find_each do |zip_sub|
-      locations = Location.where('addr2 LIKE ?', "%#{zip_sub.zip}%")
-      message = ['Appointments now available at:', nil]
-      locations.each do |clinic|
-        message << "#{clinic.name} (#{clinic.addr1}, #{clinic.addr2}). Check eligibility and sign-up at #{clinic.link}"
-        message << nil
-        clinics += 1
-      end
+    results = parse_user_zips(clinics: 0, users: 0)
+    { clinics: results[:clinics], users: results[:users] }
+  end
 
-      TWITTER_CLIENT.create_direct_message(zip_sub.user_id, message * "\n")
+  private
+
+  def parse_matching_locations(clinics:, message:, user_zip:)
+    Location.where('addr2 LIKE ?', "%#{user_zip.zip}%").each do |clinic|
+      message << "#{clinic.name} (#{clinic.addr1}, #{clinic.addr2}). Check eligibility and sign-up at #{clinic.link}"
+      message << nil
+      clinics += 1
+    end
+  end
+
+  def parse_user_zips(clinics:, users:)
+    UserZip.find_each do |user_zip|
+      parse_matching_locations(clinics: clinics, message: ['Appointments now available at:', nil], user_zip: user_zip)
+      TWITTER_CLIENT.create_direct_message(user_zip.user_id, message * "\n")
       users += 1
     end
     { clinics: clinics, users: users }

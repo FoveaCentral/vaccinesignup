0532dc1dcb004e3610bd29ce4627ddc18c37438d
Roderick Monje <rod@foveacentral.com>
2021-03-12 00:40:57 -0500

    fix: warnings

--- app/services/direct_message_reader.rb
+++ app/services/direct_message_reader.rb
@@ -6,15 +6,15 @@ class DirectMessageReader < ApplicationService
     stopped = 0
     subscribed = 0
     TWITTER_CLIENT.direct_messages_received.reverse.each do |dm|
-      next if ReadDirectMessage.exists?(direct_message_id: dm[:id])
+      next if ReadDirectMessage.exists?(direct_message_id: dm.id)
 
-      if !(dm[:text] =~ /^[0-9]{5}$/).nil?
-        sub = UserZip.create_or_find_by(user_id: dm[:sender_id], zip: dm[:text])
+      if !(dm.text =~ /^[0-9]{5}$/).nil?
+        sub = UserZip.create_or_find_by(user_id: dm.sender_id, zip: dm.text)
         subscribed += 1 if sub.persisted?
-      elsif dm[:text].downcase == 'stop'
-        stopped += UserZip.where(user_id: dm[:sender_id]).delete_all
+      elsif dm.text.downcase == 'stop'
+        stopped += UserZip.where(user_id: dm.sender_id).delete_all
       end
-      ReadDirectMessage.create(direct_message_id: dm[:id])
+      ReadDirectMessage.create(direct_message_id: dm.id)
     end
     { stopped: stopped, subscribed: subscribed }
   end

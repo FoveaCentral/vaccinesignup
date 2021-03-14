d425294fce3270ac4c129c2d75fece2d15fbd49c
Roderick Monje <rod@foveacentral.com>
2021-03-13 22:33:27 -0500

    refactor: retracted LocationsController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@
 
 # Superclass for all controllers.
 class ApplicationController < ActionController::Base
-  # Parse direct messages for zip codes.
-  def read_direct_messages
+  # Read direct messages for users that have subscribed to zip codes.
+  def read_dms
     results = DirectMessageReader.call
     render plain: "Users DMd #{results[:subscribed]} new zips and stopped #{results[:stopped]} zips."
   end
@@ -12,5 +12,13 @@ class ApplicationController < ActionController::Base
   def notify_users
     results = Notifier.call
     render plain: "Notified #{results[:users]} users about #{results[:clinics]} appointments."
+  end
+
+  # Syncs vaccine Locations with LA County's data set.
+  def sync_locations
+    results = LocationSyncer.call
+    render plain: ["Parsed #{results[:total]} locations.",
+                   "Created #{results[:new]} locations.",
+                   "Updated #{results[:updated]} locations."].join("\n")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@
 
 Rails.application.routes.draw do
   get 'notify_users' => 'application#notify_users'
-  get 'read_direct_messages' => 'application#read_direct_messages'
-  resources :locations, only: nil do
-    collection do
-      get 'sync'
-    end
-  end
+  get 'read_dms' => 'application#read_dms'
+  get 'sync_locations' => 'application#sync_locations'
 end

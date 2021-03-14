3a04ef8e7d8d3d33dccb47c94fc984b40257029c
Roderick Monje <rod@foveacentral.com>
2021-03-14 15:54:45 -0400

    test: captured bug “empty appointment-DM”
    https://github.com/ivanoblomov/vaccine-notifier/issues/1

--- spec/services/notifier_spec.rb
+++ spec/services/notifier_spec.rb
@@ -12,6 +12,31 @@ describe Notifier do
 
     let(:user_zips) { [UserZip.new(user_id: 1, zip: '90210')] }
 
-    it { should eq({ clinics: 1, users: 1 }) }
+    context 'when a user subscribes to a matching Location' do
+      let(:user_zips) { [UserZip.new(user_id: 1, zip: '90210')] }
+
+      it { should eq({ clinics: 1, users: 1 }) }
+
+      describe 'TWITTER_CLIENT' do
+        subject { TWITTER_CLIENT }
+
+        after { Notifier.call(user_zips) }
+
+        it { is_expected.to receive(:create_direct_message) }
+      end
+    end
+    context 'when a user subscribes to a non-existing Location' do
+      let(:user_zips) { [UserZip.new(user_id: 1, zip: '90044')] }
+
+      it { should eq({ clinics: 0, users: 0 }) }
+
+      describe 'TWITTER_CLIENT' do
+        subject { TWITTER_CLIENT }
+
+        after { Notifier.call(user_zips) }
+
+        it { is_expected.not_to receive(:create_direct_message) }
+      end
+    end
   end
 end

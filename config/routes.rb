0a8e255beeaf9bfb1035987f72adaf545762b260
Roderick Monje <rod@foveacentral.com>
2021-03-14 17:37:44 -0400

    fix?: heroku

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'notify_users' => 'application#notify_users'
   get 'read_dms' => 'application#read_dms'
   get 'sync_locations' => 'application#sync_locations'
+  root to: redirect('https://twitter.com/vaccinesignup/')
 end

405e8ce337827de68d6ab2150a239b1559112a15
Roderick Monje <ivanoblomov@users.noreply.github.com>
2021-03-22 16:23:36 -0400

    doc: Location, LocationSyncer, SyncBot
    * doc: LocationSyncer.new, LocationSyncer#call
    
    * doc: Location.find_by_best_key
    
    * doc: SyncBot#call

--- app/models/location.rb
+++ app/models/location.rb
@@ -11,6 +11,13 @@ class Location < ApplicationRecord
   alias_attribute :commentsSpn, :comments_spn
   alias_attribute :clinicFormat, :clinic_format
 
+  # Finds the Location matching the specified ID or street address.
+  #
+  # @param id [String] the Location's ID
+  # @param address1 [String] the Location's street address line 1
+  # @return [Location] the matching Location
+  # @example
+  #   Location.find_by_best_key('1', '1261 W 79th Street')
   def self.find_by_best_key(id, address1)
     Location.where('id = ? OR addr1 = ?', id.to_i, address1).limit(1).find_each.first
   end

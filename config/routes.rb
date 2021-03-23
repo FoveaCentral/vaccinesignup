67122ae13f492c85a927745e686a84a1656aa809
Roderick Monje <rod@foveacentral.com>
2021-03-22 23:43:12 -0400

    fix: removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'notify_users' => 'application#notify_users'
-  get 'read_dms' => 'application#read_dms'
-  get 'sync_locations' => 'application#sync_locations'
   root to: redirect('https://twitter.com/vaccinesignup/')
 end

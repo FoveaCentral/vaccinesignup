4f7370a01ce908d60e95ba14ec35abd44388bb95
Roderick Monje <rod@foveacentral.com>
2021-03-23 18:11:20 -0400

    spec: covered nil case

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -3,18 +3,21 @@
 require "#{File.dirname(__FILE__)}/../spec_helper"
 describe Location do
   describe '.find_by_best_key' do
-    subject { Location.find_by_best_key(la_id: la_id, address1: address1) }
+    subject { Location.find_by_best_key(address1: address1, la_id: la_id) }
 
     let(:address1) { nil }
     let(:la_id) { nil }
     let(:location) { FactoryBot.create(:location) }
 
-    context 'with present la_id' do
+    context 'with nil :address1 and :la_id' do
+      it { should eq nil }
+    end
+    context 'with present :la_id' do
       let(:la_id) { '5462' }
 
       it { should eq location }
     end
-    context 'with present address1' do
+    context 'with present :address1' do
       let(:address1) { '300 North Canon Drive' }
 
       it { should eq location }

cfd771859dc84ebda7aad8a749d734ab89298cbb
Roderick Monje <ivanoblomov@users.noreply.github.com>
2021-04-02 00:40:44 -0400

    fix: users can follow multiple zips (#56)
    * spec: covered bug
    
    * fix: passed
    
    * spec: updated contexts

---
+++ spec/models/user_zip_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# require "#{File.dirname(__FILE__)}/../spec_helper"
+
+describe UserZip do
+  context 'when a user already follows a zip' do
+    before { UserZip.create(user_id: 1, zip: '90044') }
+
+    context 'when following another zip' do
+      let(:user_zip) { UserZip.new(user_id: 1, zip: '90210') }
+
+      describe '#save' do
+        subject { user_zip.save }
+
+        it { should be true }
+      end
+    end
+  end
+end

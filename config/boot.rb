7e592e687b59bacf270aedbef11887e94f8c19fd
Roderick Monje <rod@foveacentral.com>
2022-04-21 13:22:43 -0400

    fix: rubocop

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Style/FetchEnvVar
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+# rubocop:enable Style/FetchEnvVar
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.

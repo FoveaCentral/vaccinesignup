b0949395c5d133dbd130672878ca23e1c6bbf935
Roderick Monje <rod@foveacentral.com>
2024-11-04 18:47:39 -0800

    fix: rubocop

--- app/models/user_zip.rb
+++ app/models/user_zip.rb
@@ -2,5 +2,7 @@
 
 # Logs which users are subscribed to which zip codes.
 class UserZip < ApplicationRecord
+  # rubocop:disable Rails/UniqueValidationWithoutIndex
   validates :user_id, uniqueness: { scope: :zip }
+  # rubocop:enable Rails/UniqueValidationWithoutIndex
 end
